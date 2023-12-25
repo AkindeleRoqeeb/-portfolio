167acd3a6d1eb81f72f52be3cf7e86b5952bad70
AkindeleRoqeeb <roqeebakindele@gmail.com>
2023-12-24 23:03:44 -0800

    newprofile

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-green-700">
+        Hello world! life man
+      </h1>
+      <h1 className="text-3xl font-bold bg-red-600 text-yellow-700">
+        Hello world! life man
+    </h1>
+    </div>
+  )
+}
